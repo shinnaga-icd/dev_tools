8c73e3fd275227b2ee97f95225b9850f2316cad2
shinji.nagasaki <shinji.nagasaki@infcurion.com>
2021-03-19 00:25:40 +0900

    add outputpdf

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/jung-kurt/gofpdf"
 )
 
 // Menu はindexページのメニュー構造体型
@@ -33,8 +34,11 @@ func main() {
 
 	// index page
 	serve.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
-			"menus": []Menu{{"リクエスト確認さん", "/request"}, {"暗号さん・複合さん", "/encrypt"}},
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"menus": []Menu{
+			{"リクエスト確認さん", "/request"},
+			{"暗号さん・複合さん", "/encrypt"},
+			{"PDF出し太郎", "/pdf"},
+		},
 		})
 	})
 
@@ -81,7 +85,38 @@ func main() {
 		})
 	})
 
+	// outputpdf
+	serve.GET("/pdf", func(c *gin.Context) {
+		download := c.Query("download")
+		log.Printf(download)
+		if download == "pdf" {
+			log.Print("test1")
+			downloadPdf(c.Writer, c.Request)
+		}
+
+		c.HTML(http.StatusOK, "outputpdf.tmpl.html", gin.H{})
+	})
+	serve.POST("/pdf", func(c *gin.Context) {
+		downloadPdf(c.Writer, c.Request)
+	})
+
 	serve.Run(":" + port)
+}
+
+func downloadPdf(w http.ResponseWriter, r *http.Request) {
+	//make pdf
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, "Hello world")
+	err := pdf.OutputFileAndClose("hello.pdf")
+	if err != nil {
+		log.Printf("err\n")
+	}
+	//download
+	w.Header().Set("Content-Disposition", "attachment; filename=hello.pdf")
+	w.Header().Set("Content-Type", "application/pdf")
+	http.ServeFile(w, r, "hello.pdf")
 }
 
 //  暗号化 AES
